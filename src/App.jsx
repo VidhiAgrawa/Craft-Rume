7dbcae8416f44f292cbeb37b92be333b0df299c4
VidhiAgrawa <vidhiagrawal2205@gmail.com>
2025-09-25 07:52:54 +0530

    adding creaft-rume Project

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+import background from './assets/background.mp4'
+import Navbar from './Components/Navbar/Navbar'
+import Aboutus from './Components/AboutUs/Aboutus'
+import Home from './Components/Home/Home'
+
+function App() {
+
+  return (
+    <>
+    <video src={background} 
+          className='back-video' 
+          autoPlay
+          loop
+          muted
+          playsInline>
+    </video>
+      <main className="main-div">
+        <div className="navbar flex">
+          <Navbar/>
+        </div>
+        <div className="home-page flex">
+          <Home/>
+        </div>
+        {/* <div className="about-us-page flex">
+          <Aboutus/>
+        </div> */}
+        {/* <div className="steps-box flex">hello</div> */}
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/AboutUs/Aboutus.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Aboutus.css'
+function Aboutus() {
+  return (
+    <div className='aboutus'>
+      <h1>About-us</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia magni animi aliquid aperiam ullam quibusdam accusamus obcaecati illo laudantium inventore vero consectetur fugit, nisi cumque quis fuga qui iste sint!
+      Facilis accusantium consectetur doloremque nulla officiis nobis atque vel consequuntur aliquam officia laborum accusamus quam veritatis fugit animi sequi, et inventore est vero qui nam necessitatibus voluptates quidem. Optio, voluptas?
+      Deserunt iusto, eius provident voluptates vitae quibusdam velit incidunt iure praesentium voluptate saepe et tempore debitis inventore sed quidem? Aut deleniti magnam eos iure, minima distinctio fugit aspernatur nemo esse?
+      In tenetur possimus quibusdam eaque incidunt labore eveniet, ut praesentium impedit eligendi blanditiis numquam quo saepe totam illum, fugit iure consequatur quidem perferendis, asperiores accusantium pariatur iste quaerat modi. Temporibus.
+      Repellendus dolor fuga reiciendis praesentium asperiores rerum laborum doloremque dignissimos, in itaque voluptas enim eligendi eius! Reiciendis ullam obcaecati similique assumenda, optio molestiae nemo, beatae cupiditate fugit cum accusantium fugiat.</p>
+    </div>
+  )
+}
+
+export default Aboutus
